1ff678f8e50418215c60fda465d5c0df2776f895
Trevor <trevmanz94@gmail.com>
2018-04-11 20:13:58 -0700

    able to save user and user albums. need to get working with populate

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,7 @@
 "use strict"
 const express = require('express');
 const router = express.Router();
+const _ = require('underscore');
 const { User } = require('../models/user');
 const { Album } = require('../models/album');
 const { Track } = require('../models/track')
@@ -23,13 +24,18 @@ module.exports = function() {
 
   router.get('/library', (req, res) => {
     spotify.setAccessToken(req.user.accessToken.toString());
-    //console.log(req.user)
-    getTracks(50, 0, req.user);
+    //getTracks(50, 0, req.user);
+    getAlbums(20, 0, req.user);
     res.redirect('/welcome')
   })
 
-  router.get('/albums', (req, res) => {
-    res.json(req.user.albums.length)
+  router.get('/albums', async (req, res) => {
+    User.findById(req.user._id)
+        .populate('albums')
+        .exec((err, user) => {
+          if (err) console.log(err);
+          res.json(user);
+        });
   })
 
   router.get('/welcome', (req, res) => {
@@ -67,8 +73,8 @@ const getTracks = async (limit, offset, user) => {
                Album.findOneAndUpdate(albumQuery, album, options),
                Track.findOneAndUpdate(trackQuery, track, options)
              ])
-             let exists = user.albums.find(item => userAlbum._id.equals(item.ref))
-                || updateAlbums.find(item => userAlbum._id.equals(item.ref))
+             let exists = user.albums.find(item => userAlbum._id.equals(item.ref)) ||
+                updateAlbums.find(item => userAlbum._id.equals(item.ref))
              if(!exists) {
                updateAlbums.push({ date_added: track.date_added, ref: userAlbum._id })
              }
@@ -80,4 +86,43 @@ const getTracks = async (limit, offset, user) => {
              })
            });
          }).catch(err => console.log(err));
+}
+
+const getAlbums = async (limit, offset, user) => {
+  let data = await spotify.getMySavedTracks({ limit: limit, offset: offset });
+  let tracks = data.body.items.map(item => {
+    return {
+      trackID: item.track.id,
+      track: item.track,
+      albumID: item.track.album.id,
+      album: null,
+      date_added: item.added_at
+    }
+  });
+  let uniqAlbums = _.uniq(tracks, item => item.albumID);
+  let albumObjs = await spotify.getAlbums(uniqAlbums.map(album => album.albumID));
+
+  for (let i = 0; i < uniqAlbums.length; i++) {
+    uniqAlbums[i].album = albumObjs.body.albums[i]
+  }
+
+  Promise.all(uniqAlbums.map(async(album) => {
+    const albumQuery = { albumID: album.album.id };
+    const albumDB = {
+      albumID: album.albumID,
+      album: album.album,
+    };
+    const options = { upsert: true, new: true };
+
+    let albumObj = await Album.findOneAndUpdate(albumQuery, albumDB, options);
+    let exists = user.albums.find(item => albumObj._id.equals(item.ref));
+    if(!exists) {
+      await User.findById(user._id).then(user => {
+        user.albums.push({ date_added: album.date_added, ref: albumObj._id });
+        user.save();
+      })
+    }
+  })).then(() => {
+    if (tracks.length === limit) getAlbums(limit, offset+limit, user);
+  })
 }
