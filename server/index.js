cfb1cc9be8aaeb15270cfc83fb684243a5a0738b
Trevor <trevmanz94@gmail.com>
2018-04-26 11:46:26 -0700

    prep for heroku deployment; running on one port

--- server/index.js
+++ server/index.js
@@ -3,8 +3,13 @@ const morgan = require('morgan');
 const Spotify = require('node-spotify-api');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const serve = require('express-static');
+
+//import authorization routes
 const auth = require('./routes/auth');
+//import routes
 const routes = require('./routes/routes');
+//import spotify passport configuration
 const passportSpotify = require('./auth/spotify')
 
 
@@ -19,10 +24,6 @@ if (!process.env.MONGODB_URI) {
 }
 mongoose.connect(process.env.MONGODB_URI).then(() => console.log('Connected to MongoDB!'));
 
-
-
-
-///session???? is this needed???
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
@@ -35,67 +36,11 @@ app.use(passportSpotify.session());
 
 //Routes
 app.use('/', auth(passportSpotify));
-app.use('/', routes());
+app.use('/api', routes());
+
+app.use(serve(__dirname + '/../build'));
+
 
 let port = process.env.PORT || 8888
 console.log(`Listening on port ${port}. Go /auth/spotify to initiate authentication flow.`)
 app.listen(port)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// app.get('/login', function(req, res) {
-//   res.redirect('https://accounts.spotify.com/authorize?' +
-//     querystring.stringify({
-//       response_type: 'code',
-//       client_id: process.env.SPOTIFY_CLIENT_ID,
-//       scope: 'user-read-private user-read-email',
-//       redirect_uri
-//     }))
-// })
-//
-// app.get('/callback', function(req, res) {
-//   let code = req.query.code || null
-//   let authOptions = {
-//     url: 'https://accounts.spotify.com/api/token',
-//     form: {
-//       code: code,
-//       redirect_uri,
-//       grant_type: 'authorization_code'
-//     },
-//     headers: {
-//       'Authorization': 'Basic ' + (new Buffer(
-//         process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
-//       ).toString('base64'))
-//     },
-//     json: true
-//   }
-//   request.post(authOptions, function(error, response, body) {
-//     var access_token = body.access_token
-//     let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
-//     res.redirect(uri + '?access_token=' + access_token)
-//   })
-// })
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,14 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = function(passport) {
-  router.get('/auth/spotify',
+  router.get('/api/auth/spotify',
   passport.authenticate('spotify', {scope: ['user-read-email', 'user-library-read', 'playlist-modify-public', 'playlist-modify-private' ] }),
   function(req, res){
+    console.log('inside auth')
     // The request will be redirected to spotify for authentication, so this
     // function will not be called.
   });
 
-  router.get('/callback',
+  router.get('/api/callback',
   passport.authenticate('spotify', { failureRedirect: '/' }),
   function(req, res) {
     // Successful authentication, redirect home.
